1b4f1d40d8fa8e075ad8685e851176883ac3c734
Tobias Brink <brink@mm.tu-darmstadt.de>
2017-04-20 15:58:05 +0200

    Add timing info to test run.

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running tests..."
 echo
 echo "This could take a while. Only errors are shown. If the output"
 echo "is empty, all tests passed."
 echo
-python3 run_tests.py | grep -B1 '!!!'
+time python3 run_tests.py | grep -B1 '!!!'
